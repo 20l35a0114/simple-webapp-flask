d83f565e89565a889309e59cfd8ad0c8b0ed6d89
uday <udaykiranoleti30@gmail.com>
2023-10-12 14:46:04 +0530

    added hai

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ def main():
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
-    
-@app.route('/hai')
+
+@app.route('/who are you')
 def hello2():
-    return 'I am uday?'    
+    return 'I am uday'    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
